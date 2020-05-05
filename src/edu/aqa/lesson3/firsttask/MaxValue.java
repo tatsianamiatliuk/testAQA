b2538cd06f32e8b0564f5233a86f292f6b3f35d4
Tatsiana <qatatsiana@gmail.com>
2020-05-05 15:48:10 +0300

    Second Task

--- src/edu/aqa/lesson3/firsttask/MaxValue.java
+++ src/edu/aqa/lesson3/firsttask/MaxValue.java
@@ -1,15 +1,35 @@
 package edu.aqa.lesson3.firsttask;
+import java.util.Random;
 /* Написать программу выводящую на экран случайно сгенерированное трёхзначное натуральное число и его наибольшую цифру*/
 public class MaxValue {
-    public static void main(String[] args) {
-        int number = (new java.util.Random()).nextInt(1000);
-        int addParameter, maxValue = 0;
-        System.out.println("Generated value = " + number);
-        while (number > 0) {
-            addParameter = number % 10;
+    public static int generateNumber(){
+        return new Random().nextInt(899) + 100;
+    }
+
+    public static int[] parse(int number){
+        int[] arrayOfNumbers = new int [3];
+        for (int i = 0; i < 3; i++){
+            arrayOfNumbers[i] = number % 10;
             number = number / 10;
-            if (maxValue < addParameter) maxValue = addParameter;
         }
-        System.out.println("maxValue = " + maxValue);
+        return  arrayOfNumbers;
+    }
+
+    public static int maxValue (int [] array){
+        int max = 0;
+        for (int i = 0; i < 3; i++){
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return  max;
+    }
+
+    public static void main(String[] args) {
+        int number = generateNumber();
+        System.out.println("Value = " + number);
+        int max;
+        max = maxValue(parse(number));
+        System.out.println("maxValue = " + max);
     }
 }
