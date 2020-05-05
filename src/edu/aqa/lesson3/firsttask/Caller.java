f2153c05543ef3c2117e9942d1461575b1ee292d
Tatsiana <qatatsiana@gmail.com>
2020-05-05 16:12:30 +0300

    Second Task

--- src/edu/aqa/lesson3/firsttask/Caller.java
+++ src/edu/aqa/lesson3/firsttask/Caller.java
@@ -2,7 +2,7 @@ package edu.aqa.lesson3.firsttask;
 import edu.aqa.lesson3.firsttask.Manipulator;
 import java.util.Random;
 /* Написать программу выводящую на экран случайно сгенерированное трёхзначное натуральное число и его наибольшую цифру*/
-public class MaxValue {
+public class Caller {
     public static void main(String[] args) {
         int max;
         int number;
